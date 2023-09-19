c514d09d064705eea1a461b6b07aef7c24fc8a7b
Jakub Pradeniak <jakub.pradeniak@bluenet.cz>
2023-09-19 21:38:39 +0200

    úprava js-helpers

--- js-helpers/js-helpers.js
+++ js-helpers/js-helpers.js
@@ -12,3 +12,39 @@ function safeRemove(element) {
         return element;
     }
 }
+
+/**
+ * Pomocná funkce pro vymazání obsahu selectu.
+ * @param {HTMLElement} select Element select získaný třeba pomocí document.getElementById
+ */
+function removeSelectOptions(select) {
+    while (select.firstChild) {
+        select.removeChild(select.firstChild);
+    }
+}
+
+/**
+ * Pomocná funkce pro připojení nového obsahu k již existujícímu obsahu selectu.
+ * @param {Array} dataSet Pole objektů ve formátu [{lable: "Popisek", value: "Hodnota"}, ...]
+ * @param {HTMLElement} select Element select získaný třeba pomocí document.getElementById
+ */
+function addSelectOptions(dataSet, select) {
+    dataSet.forEach((data) => {
+        const option = document.createElement('option');
+
+        option.innerText = data.label;
+        option.setAttribute("value", data.value);
+
+        select.appendChild(option);
+    });
+}
+
+/**
+ * Pomocná funkce pro nastavení obsahu selectu.
+ * @param {Array} dataSet Pole objektů ve formátu [{lable: "Popisek", value: "Hodnota"}, ...]
+ * @param {HTMLElement} select Element select získaný třeba pomocí document.getElementById
+ */
+function setSelectOptions(dataSet, select) {
+    removeSelectOptions(select);
+    addSelectOptions(dataSet, select);
+}
